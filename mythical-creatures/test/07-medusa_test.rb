669e0c7cff1529ce8a0042218c794a7f20f9f746
Nico Rithner <nicorithner@gmail.com>
2021-01-07 21:31:22 -0700

    Add new tests

--- mythical-creatures/test/07-medusa_test.rb
+++ mythical-creatures/test/07-medusa_test.rb
@@ -10,13 +10,13 @@ class MedusaTest < Minitest::Test
   end
 
   def test_when_first_created_she_has_no_statues
-    skip
+    # skip
     medusa = Medusa.new("Cassiopeia")
     assert medusa.statues.empty?
   end
 
   def test_when_staring_at_a_person_she_gains_a_statue
-    skip
+    # skip
     medusa = Medusa.new("Cassiopeia")
     victim = Person.new("Perseus")
 
@@ -26,7 +26,7 @@ class MedusaTest < Minitest::Test
   end
 
   def test_when_staring_at_a_person_that_person_turns_to_stone
-    skip
+    # skip
     medusa = Medusa.new("Cassiopeia")
     victim = Person.new("Perseus")
 
@@ -36,13 +36,36 @@ class MedusaTest < Minitest::Test
   end
 
   def test_can_only_have_three_victims
-    skip
+    # skip
     # your code here
+    medusa = Medusa.new("Cassiopeia")
+    victims = [victim_1 = Person.new("Perseus"),
+    victim_2 = Person.new("Marius"),
+    victim_3 = Person.new("Demetrius"),
+    victim_4 = Person.new("Pericles")]
+
+    victims.each do |victim|
+      medusa.stare(victim)
+    end
+    assert_equal 3, medusa.statues.count
+    assert_equal [victim_2, victim_3, victim_4], medusa.statues
   end
 
   def test_if_a_fourth_victim_is_stoned_first_is_unstoned
-    skip
+    # skip
     # your code here
+    medusa = Medusa.new("Cassiopeia")
+    victims = [victim_1 = Person.new("Perseus"),
+    victim_2 = Person.new("Marius"),
+    victim_3 = Person.new("Demetrius"),
+    victim_4 = Person.new("Pericles")]
+
+    victims.each do |victim|
+      medusa.stare(victim)
+    end
+    refute medusa.statues.include?(victim_1)
+    refute victim_1.stoned?
+    assert victim_4.stoned?
   end
 
 end
