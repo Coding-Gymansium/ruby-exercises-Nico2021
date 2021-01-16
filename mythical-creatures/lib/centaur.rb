20e0bcf2bbc867b4cd1ad6d68f7c6907a120547d
Nico Rithner <nicorithner@gmail.com>
2021-01-15 19:49:01 -0700

    Refactor and organized methods

--- mythical-creatures/lib/centaur.rb
+++ mythical-creatures/lib/centaur.rb
@@ -1,73 +1,22 @@
 class Centaur
   attr_reader :name, :breed
   def initialize(name, breed)
-    @name = name
     @breed = breed
     @cranky = false
-    @standing = true
-    @work = 0
-    @sleep = false
-    @laying = false
+    @name = name
     @rested = true
     @sick = false
-  end
-
-  def shoot
-    if self.cranky? || self.laying?
-      "NO!"
-    else 
-      @work += 1
-      "Twang!!!"
-    end
-  end
-
-  def run
-    if self.cranky? || self.laying?
-      "NO!"
-    else 
-      @work += 1
-      "Clop clop clop clop!!!"
-    end
+    @sleep = false
+    @standing = true
+    @work = 0
   end
 
   def cranky?
     @work >= 3 ? @cranky = true : @cranky = false;
   end
   
-  def standing?
-    @standing
-  end
-  
-  def sleep
-    if self.standing?
-      "NO!"
-    else 
-      @sleep
-      @work = 0
-    end
-  end
-  
-  def lay_down
-    @laying = true
-    @standing = false
-  end
-  
-  def laying?
-    @laying == true
-    @standing == false
-  end
-  
-  def stand_up
-    @standing = true
-    @lay_down = false
-  end
-
-  def rested?
-    @work >= 3 ? @rested = false : @rested = true;
-  end
-
   def drink_potion
-    if self.laying?
+    if !self.standing?
       "NO!"
     elsif self.rested?
       @sick = true
@@ -77,8 +26,56 @@ class Centaur
       "Gloo, Gloo, Gloo"
     end
   end
+  
+  def lay_down
+    @standing = false
+  end
+  
+  def laying?
+    @standing == false
+  end
+
+  def rested?
+    @work >= 3 ? @rested = false : @rested = true;
+  end
+
+  def run
+    string = "Clop clop clop clop!!!"
+    self.will_work(string)
+  end
 
   def sick?
     @sick
+  end
+
+  def shoot
+    string = "Twang!!!"
+    self.will_work(string)
+  end
+
+  def sleep
+    if self.standing?
+      "NO!"
+    else 
+      @sleep
+      @work = 0
+    end
+  end
+
+  def stand_up
+    @standing = true
+  end
+
+  def standing?
+    @standing
+  end
+
+  def will_work(string)
+    if self.cranky? || self.laying?
+      "NO!"
+    else 
+      @work += 1
+      string
+    end
   end
 end
